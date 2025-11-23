bb6c980ab3f3434620f78662af4d181f2b032096
pari25bce10209-code <pari.25bce10209@vitbhopal.ac.in>
2025-11-23 14:44:15 +0530

    Enhance main.py with comments for better understanding
    Added comments for clarity and improved readability.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # main.py
 from sales_manager import setupFile, addSale, viewSales, sortByDate, showSummary,updateSale,deleteSale
-from pandas_helper import showChart
+from pandas_helper import showChart          #importing different functions from the modules we created before
 
-def menu():
+def menu(): #showing menu to the user
     print("\n--- Sales Manager ---")
     print("1. Add Sale")
     print("2. View Sales")
@@ -17,13 +17,13 @@ def main():
     setupFile()
     while True:
         menu()
-        choice = input("Enter choice: ")
-        if choice == "1":
+        choice = input("Enter choice: ") #entering the choice of the user
+        if choice == "1": #matching the choice entered by user to know which function is to be used
             product = input("Product: ")
             qty = int(input("Quantity: "))
             price = float(input("Price: "))
             date = input("Date (YYYY-MM-DD): ")
-            addSale(product, qty, price, date)
+            addSale(product, qty, price, date) #calling the function
         elif choice == "2":
             viewSales()
         elif choice == "3":
@@ -43,10 +43,11 @@ def main():
             showSummary()
         elif choice == "7":
             showChart()
-        elif choice=="8":
+        elif choice=="8": #to enter correct choice
             print ("TRY AGAIN")
             break
         
 
 if __name__ == "__main__":
+
     main()
